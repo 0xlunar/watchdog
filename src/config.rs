c6932ed5c44a0e63f9b8d32327b6bdf51142f5f2
0xlunar <20531468+0xlunar@users.noreply.github.com>
2024-04-09 22:21:32 +1000

    Initial Commit

---
+++ src/config.rs
@@ -0,0 +1,153 @@
+use std::collections::HashMap;
+use std::ffi::OsString;
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process::Child;
+use std::rc::Rc;
+use std::sync::Mutex;
+use std::time::{Duration, UNIX_EPOCH};
+use crate::CommandLineArguments;
+
+pub struct Config {
+    directory: PathBuf,
+    file_name: OsString,
+    process: Option<Child>,
+    pid: Option<usize>,
+    exit_code: Option<isize>,
+    watch_files: bool,
+    only_non_zero_exit: bool,
+    restart_delay: usize,
+    recheck_delay: usize,
+}
+
+impl Config {
+    pub fn start(&mut self) {
+        let mut full_path = self.directory.clone();
+        full_path.push(self.file_name.as_os_str());
+
+
+        if !full_path.try_exists().unwrap() {
+            panic!("File doesn't exist")
+        }
+        println!("Fullpath: {:?}", full_path.as_os_str());
+
+        // TODO: Fix bug causing processess to not actually launch? and valid existing files not being found/runnable
+        let process = std::process::Command::new(full_path)
+            .spawn().expect("Cannot spawn process");
+
+        self.process = Some(process);
+    }
+    pub fn stop(&mut self) -> bool {
+        let success = match &mut self.process {
+            None => {
+                println!("No process");
+                false
+            },
+            Some(c) => match c.kill() {
+                Ok(_) => true,
+                Err(e) => {
+                    println!("Failed to terminate process: {}", e);
+                    false
+                },
+            }
+        };
+
+        if success {
+            self.process = None;
+        }
+
+        success
+    }
+
+    pub fn directory(&self) -> &Path {
+        self.directory.as_path()
+    }
+    pub fn restart_delay(&self) -> usize {
+        self.restart_delay
+    }
+    pub fn recheck_delay(&self) -> usize {
+        self.recheck_delay
+    }
+
+    pub fn check_file_changes(directory: &Path, cache: Rc<Mutex<HashMap<OsString, u64>>>) -> bool {
+        let paths = fs::read_dir(directory).unwrap();
+        let mut file_updated = false;
+        let mut cache_lock = cache.lock().unwrap();
+        for path in paths {
+            let path = path.unwrap();
+            let file_name = path.file_name();
+            let metadata = path.metadata().unwrap();
+            let value = match metadata.modified() {
+                // If System supports modified time we use the time, otherwise just use file size. Will miss if change resulted in exact same file size
+                Ok(t) => t.duration_since(UNIX_EPOCH).unwrap().as_secs(),
+                Err(_) => metadata.len()
+            };
+
+            cache_lock.entry(file_name).and_modify(|v| {
+                if *v != value {
+                    file_updated = true;
+                    *v = value;
+                }
+            }).or_insert(value);
+
+            if file_updated {
+                break;
+            }
+        }
+
+        file_updated
+    }
+    pub fn check_process_exit(&mut self) {
+        match &mut self.process {
+            Some(c) => match &c.try_wait() {
+                Ok(t) => match t {
+                    Some(es) => {
+                        println!("Process Exited: {:?}", es);
+                        if es.success() && !self.only_non_zero_exit {
+                            if self.restart_delay > 0 {
+                                std::thread::sleep(Duration::from_millis(self.restart_delay as u64));
+                            }
+                            self.start()
+                        } else if !es.success() {
+                            if self.restart_delay > 0 {
+                                std::thread::sleep(Duration::from_millis(self.restart_delay as u64));
+                            }
+                            self.start()
+                        }
+                    },
+                    None => ()
+                },
+                Err(e) => println!("Error waiting: {}", e)
+            },
+            None => ()
+        }
+    }
+}
+
+
+
+impl From<CommandLineArguments> for Config {
+    fn from(item: CommandLineArguments) -> Self {
+        let mut directory = PathBuf::from(item.path);
+        let file_name = match directory.extension() {
+            Some(_) => {
+                let stem = directory.file_name().unwrap().to_os_string();
+                directory.pop();
+                stem
+            },
+            None => panic!("Invalid file path, eg \"./My Application\\app.exe\" or \"C:\\Users\\You\\Desktop\\My Application\\app.exe\"")
+        };
+
+        Self {
+            directory,
+            file_name,
+            process: None,
+            pid: None,
+            exit_code: None,
+            watch_files: item.watch_files,
+            only_non_zero_exit: item.only_non_zero_exit,
+            restart_delay: item.restart_delay,
+            recheck_delay: item.recheck_delay,
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,77 @@
+mod config;
+
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use clap::Parser;
+use crate::config::Config;
+
+#[derive(Debug, Parser)]
+pub struct CommandLineArguments {
+    #[arg(short, long, help = "Process executable path", required = true)]
+    pub path: String,
+    #[arg(short = 'w', long = "watchFiles", default_value_t = false, help = "Restart application on file changes in directory/subdirectories")]
+    pub watch_files: bool,
+    #[arg(short = 'z', long = "onlyNonZeroExit", default_value_t = false, help = "Only exit on a non-zero status code")]
+    pub only_non_zero_exit: bool,
+    #[arg(short = 'r', long = "restartDelay", default_value_t = 0, help = "Delay before restarting process in ms")]
+    pub restart_delay: usize,
+    #[arg(short = 'c', long = "recheckDelay", default_value_t = 0, help = "How often the process is checked in ms")]
+    pub recheck_delay: usize,
+    #[arg(long = "forceRestartDelay", default_value_t = 0, help = "Forces a restart after a delay in ms [0 = Disabled]")]
+    pub force_restart_delay: usize,
+}
+
+
+fn main() {
+    let args = CommandLineArguments::parse();
+    let mut config: Config = args.into();
+    let recheck_delay = config.recheck_delay() as u64;
+    let restart_delay = config.restart_delay() as u64;
+    let dir = config.directory().to_path_buf();
+    println!("Starting Process");
+
+    config.start();
+
+    let config = Arc::new(Mutex::new(config));
+    let e_config = Arc::clone(&config);
+
+    let t_config = Arc::clone(&config);
+    let file_thread = std::thread::spawn(move || {
+        let cache = Rc::new(Mutex::new(HashMap::new()));
+        loop {
+            {
+                let changes = Config::check_file_changes(dir.as_path(), Rc::clone(&cache));
+                if changes {
+                    println!("File changes detected!");
+                    let mut lock = t_config.lock().unwrap();
+                    if lock.stop() {
+                        if restart_delay > 0 {
+                            std::thread::sleep(Duration::from_millis(restart_delay));
+                        }
+                        lock.start()
+                    }
+                }
+            }
+            if recheck_delay > 0 {
+                std::thread::sleep(Duration::from_millis(recheck_delay));
+            }
+        }
+    });
+
+    let t_config = Arc::clone(&e_config);
+    let aliveness_task = std::thread::spawn(move || {
+        loop {
+            {
+                t_config.lock().unwrap().check_process_exit();
+            }
+            std::thread::sleep(Duration::from_millis(recheck_delay));
+        }
+    });
+
+    let _ = file_thread.join();
+    let _ = aliveness_task.join();
+
+    e_config.lock().unwrap().stop();
+}
